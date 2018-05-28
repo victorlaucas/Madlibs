9343f4a487ce1633955e1e03f9e6cbd430dfdddd
Victor <victorlaucas@gmail.com>
2018-05-27 20:40:09 -0600

    Added react strap and inputs

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,13 +1,62 @@
 import React, { Component } from 'react';
+import {
+    Col,
+    Row
+} from 'reactstrap';
 
 class MadlibForm extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Form component</h1>
-      </div>
-    );
-  }
+
+    handleChange = function() {
+        console.log('trying to handle change');
+    }
+
+
+    render() {
+      return (
+        <Row style={{textAlign: 'center', color: 'white'}}>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Color' type='text' onChange={this.handleChange} />
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Color' type='text' onChange={this.handleChange}  />
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Color' type='text' onChange={this.handleChange}  />
+                    </Col>
+                </Row>
+            </Col>
+            <Col md="3" className="inputWrapper">
+                <Row>
+                    <Col md="2">
+                        <label className="greenLabel">1</label>
+                    </Col>
+                    <Col md="10">
+                        <input placeholder='Color' type='text' onChange={this.handleChange} />
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+      );
+    }
 }
 
 export default MadlibForm;
