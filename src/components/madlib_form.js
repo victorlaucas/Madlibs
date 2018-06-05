facb522e2c5575f5fef3fe7c373ff5ec735f9055
Victor <victorlaucas@gmail.com>
2018-06-05 11:37:09 -0600

    Implemented clear button that resets  madlib_form state. Styled buttons

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -60,6 +60,27 @@ class MadlibForm extends Component {
       event.preventDefault();
     }.bind(this);
 
+    handleClick = function() {
+      this.setState({
+            completedForm: false,
+            color: '',
+            pluralNoun: '',
+            adjectiveOne: '',
+            celebrityOne: '',
+            adjectiveTwo: '',
+            nounOne: '',
+            numberOne: '',
+            numberTwo: '',
+      });
+    }.bind(this)
+
+    renderButton = function() {
+      if (this.state.completedForm) {
+        return <a className="clear-button" onClick={this.handleClick}>Clear MadLib</a>
+      }
+      return <input type="submit" className="generate-button" value="Generate Madlib"/>
+    }
+
     render() {
 
         this.inputData = [
@@ -88,7 +109,7 @@ class MadlibForm extends Component {
                 </Row>
                 <Row>
                   <Col md="12" className="button-wrapper">
-                    <input type="submit" className="generate-button" value="Generate Madlib"/>
+                    {this.renderButton()}
                   </Col>
                 </Row>
               </form>
